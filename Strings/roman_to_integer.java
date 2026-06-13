2d786e3292044c4ea9338450aff5f9e9bc334283
Asim Yash <yashraj14121@gmail.com>
2026-06-13 18:02:01 +0530

    Create roman_to_integer.java
    Added for roman to integer
    
    leetcode #13

---
+++ Strings/roman_to_integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0, p = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            int c = getValue(s.charAt(i));
+            if (c < p)
+            total = total-c;
+            else
+            total = total+c;
+            p = c;
+        }
+        return total;
+    }
+    private int getValue(char ch){
+        switch (ch){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+}
