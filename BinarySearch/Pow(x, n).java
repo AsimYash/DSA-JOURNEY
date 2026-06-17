40fdbcdbd8e6bbc74c1c728ff6ff94b28ac71909
Asim Yash <yashraj14121@gmail.com>
2026-06-17 16:45:25 +0530

    Create Pow(x, n).java
    Added for pow(x, n) 
    
    leetcode #50

---
+++ BinarySearch/Pow(x, n).java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double r = 1;
+        while (N > 0) {
+            if ((N % 2) == 1)
+            r *= x;
+            x *= x;
+            N /= 2;
+        }
+        return r;
+    }
+}
