96c832639223881b43670f6936dcb987e1f853ea
Asim Yash <yashraj14121@gmail.com>
2026-06-19 17:50:13 +0530

    Create odd_even_linkedlist.java
    Added for odd even linked list
    
    leetcode #328

---
+++ LinkedLists/odd_even_linkedlist.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null)
+        return head;
+        ListNode o = head;
+        ListNode e = head.next;
+        ListNode eh = e;
+        while (e != null && e.next != null) {
+            o.next = e.next;
+            o = o.next;
+            e.next = o.next;
+            e = e.next;
+        }
+        o.next = eh;
+        return head;
+    }
+}
