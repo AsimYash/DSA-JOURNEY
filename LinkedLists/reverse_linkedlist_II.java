8f05ab4bd43151c63f513183dffb4470e055e43e
Asim Yash <yashraj14121@gmail.com>
2026-06-20 17:50:47 +0530

    Create reverse_linkedlist_II.java
    Added for reverse linked list II
    
    leetcode #92

---
+++ LinkedLists/reverse_linkedlist_II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right)
+        return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        for (int i = 1; i < left; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
+        ListNode connection = prev;
+        ListNode tail = curr;
+        for (int i = 0; i < right - left + 1; i++) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        if (connection != null)
+        connection.next = prev;
+        else
+        head = prev;
+        tail.next = curr;
+        return head;
+    }
+}
