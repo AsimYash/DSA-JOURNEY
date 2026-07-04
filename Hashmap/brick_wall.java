33e242fc5f7402bf718ff4f8dd147a6312e29b9a
Asim Yash <yashraj14121@gmail.com>
2026-07-04 21:48:52 +0530

    Create brick_wall.java
    Added for brick wall
    
    leetcode #554

---
+++ Hashmap/brick_wall.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxEdges = 0;
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                int count = map.getOrDefault(sum, 0) + 1;
+                map.put(sum, count);
+                maxEdges = Math.max(maxEdges, count);
+            }
+        }
+        return wall.size() - maxEdges;
+    }
+}
