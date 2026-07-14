ef344df2a4aa16a0cac7b3406446defa9369c589
Asim Yash <yashraj14121@gmail.com>
2026-07-14 22:28:20 +0530

    Create check_if_array_pairs_are_divisible_by_k.java
    Added for checking if array pairs are divisible by k
    
    leetcode #1497

---
+++ Hashmap/check_if_array_pairs_are_divisible.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr){
+            int rem = ((num % k) + k) % k;
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+        for (int rem : map.keySet()){
+            if (rem == 0){
+                if (map.get(rem) % 2 != 0)
+                return false;
+            }
+            else if (2 * rem == k){
+                if (map.get(rem) % 2 != 0)
+                return false;
+            }
+            else{
+                if (map.get(rem).intValue() != map.getOrDefault(k - rem, 0))
+                return false;
+            }
+        }
+        return true;
+    }
+}
